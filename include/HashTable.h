ded09ae822bd8b7123abbed59137cca49dea0a77
DiegoDiazMoron <alu0101337975@ull.edu.es>
2022-04-03 13:10:54 +0100

    Main a medias

--- include/HashTable.h
+++ include/HashTable.h
@@ -4,16 +4,24 @@
 
 #ifndef P04DIEGODIAZMORON_HASHTABLE_H
 #define P04DIEGODIAZMORON_HASHTABLE_H
+
+#include "DispersionFunction.h"
+#include "ExplorationFunction.h"
+
 template<class Key>
 class HashTable {
 public:
-    HashTable(int size);
+    HashTable(int size, DispersionFunction<Key> *dispersionFunction, ExplorationFunction<Key> *explorationFunction = nullptr, int blockSize = 0);
     ~HashTable();
     void insert(Key key);
     bool search(Key key);
 private:
     int tableSize;
     Key *table;
+    DispersionFunction<Key> *fd;
+    ExplorationFunction<Key> *fe;
+
+
 };
 #endif //P04DIEGODIAZMORON_HASHTABLE_H
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,51 @@
 #include <iostream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    int tableSize, fd, td;
+    std::cout << "Introduce el tamano de la tabla:" << std::endl;
+    std::cin >> tableSize;
+    std::cout << "Seleccione una funcion de dispersion: \n"
+                 "  1.- Modulo \n"
+                 "  2.- Basada en la Suma \n"
+                 "  3.- Pseudoaleatoria " << std::endl;
+    std::cin >> fd;
+    switch (fd) {
+        case 1:
+            std::cout << "Modulo con abierta" << std::endl;
+            break;
+        case 2:
+            std::cout << "Basada en la suma con abierta" << std::endl;
+            break;
+        case 3:
+            std::cout << "Pseudoaleatoria con abierta" << std::endl;
+            break;
+        default:
+            std::cout << "Opcion invalida" << std::endl;
+            return 1;
+    };
+    std::cout << "Seleccione una tecnica de dispersion: \n"
+                 "  1.- Abierta \n"
+                 "  2.- Cerrada" << std::endl;
+    std::cin >> td;
+    switch (td) {
+        case 1:
+            std::cout << "Modulo con abierta" << std::endl;
+            break;
+        case 2:
+            std::cout << "Modulo con cerrada" << std::endl;
+            int blockSize, fe;
+            std::cout << "Introduce el tamano del bloque:" << std::endl;
+            std::cin >> blockSize;
+            std::cout << "Seleccione una funcion de exploracion: \n"
+                         "  1.- Lineal \n"
+                         "  2.- Cuadratica \n"
+                         "  3.- Doble dispersion \n"
+                         "  4.- Redispersion " << std::endl;
+            std::cin >> fe;
+            break;
+        default:
+            std::cout << "Opcion invalida" << std::endl;
+            return 1;
+    }
     return 0;
 }
