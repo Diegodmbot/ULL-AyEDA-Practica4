7340e7fde866f3ea0e5f6028e9170c06bb17dbe1
DiegoDiazMoron <alu0101337975@ull.edu.es>
2022-04-05 21:45:21 +0100

    Completo

---
+++ CMakeFiles/P04DiegoDiazMoron.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/P04DiegoDiazMoron.dir/src/main.o"
+  "P04DiegoDiazMoron.pdb"
+  "P04DiegoDiazMoron"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/P04DiegoDiazMoron.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
